df3512011f36f72639a8ed051eda4f0623e735f2
Anne Edmondson <edmondson.anne@gmail.com>
2019-04-29 05:08:33 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -9,7 +9,7 @@ class MusicImporter
     Dir.glob("#{path}/*.mp3").collect {|file| file.gsub("#{path}/", "")}
   end
 
-  def import(files)
+  def import(file_name)
     files.each do |filename|
       Song.create_from_filename(file_name)
     end
