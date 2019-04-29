7c5667b07adab11847cd540709ffc5cee156ecbf
Anne Edmondson <edmondson.anne@gmail.com>
2019-04-29 04:01:04 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,9 @@ module Concerns
     def find_by_name(name)
       self.all.detect {|obj| obj.name == name}
     end
+
+    def find_or_create_by_name(name)
+      find_by_name(name) || create(name)
   end
 end
 
