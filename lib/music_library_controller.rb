627b8fd32c9053870aa087c2f8df37dd0ae40697
Anne Edmondson <edmondson.anne@gmail.com>
2019-04-29 05:20:30 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -5,19 +5,19 @@ class MusicLibraryController
     MusicImporter.new(path).import
   end
 
-  input = gets.strip
-
+  def call
+   input = gets.strip
    if input != exit
 
-   puts "Welcome to your music library!"
-   puts "To list all of your songs, enter 'list songs'."
-   puts "To list all of the artists in your library, enter 'list artists'."
-   puts "To list all of the genres in your library, enter 'list genres'."
-   puts "To list all of the songs by a particular artist, enter 'list artist'."
-   puts "To list all of the songs of a particular genre, enter 'list genre'."
-   puts "To play a song, enter 'play song'."
-   puts "To quit, type 'exit'."
-   puts "What would you like to do?"
+     puts "Welcome to your music library!"
+     puts "To list all of your songs, enter 'list songs'."
+     puts "To list all of the artists in your library, enter 'list artists'."
+     puts "To list all of the genres in your library, enter 'list genres'."
+     puts "To list all of the songs by a particular artist, enter 'list artist'."
+     puts "To list all of the songs of a particular genre, enter 'list genre'."
+     puts "To play a song, enter 'play song'."
+     puts "To quit, type 'exit'."
+     puts "What would you like to do?"
 
    case input
        when "list songs"
@@ -34,6 +34,7 @@ class MusicLibraryController
          play_song
        end
    end
+ end
 
 
   def list_songs
