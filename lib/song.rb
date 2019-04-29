dca84469cb329b3db6b73c64b9b686364c49073e
Anne Edmondson <edmondson.anne@gmail.com>
2019-04-29 04:49:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,12 +51,12 @@ class Song
     artist_name, song_name, genre_name = names[0], names[1], names[2].gsub(".mp3", "")
 
     new_artist = Artist.find_or_create_by_name(artist_name)
-    new_genre = Genre,find_or_create_by_name(genre_name)
+    new_genre = Genre.find_or_create_by_name(genre_name)
     new_song = Song.new(song_name, new_artist, new_genre)
   end
 
   def self.create_from_filename(file_name)
-    song.new_from_filename(file_name).tap { |song| song.save}
+    song_new_from_filename(file_name).tap { |song| song.save}
   end
 
 end
