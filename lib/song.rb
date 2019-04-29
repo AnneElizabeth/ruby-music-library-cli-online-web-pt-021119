cb1c96e1aec0ce42bdf5531a5366ba8f50a7bd0d
Anne Edmondson <edmondson.anne@gmail.com>
2019-04-29 05:02:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
     find_by_name(song_name) || create(song_name)
   end
 
-  def self.song_new_from_filename(file_name)
+  def self.new_from_filename(file_name)
     names = file_name.split(" - ")
     artist_name, song_name, genre_name = names[0], names[1], names[2].gsub(".mp3", "")
 
